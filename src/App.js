6d9ca6d13b081021e768ad25f9c8c5ebbd7040ca
Adam Spindler <xmeadamx@gmail.com>
2020-01-17 07:57:23 -0800

    Initial commit
    Most of this code is copied from the react-table examples

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import MyTable from './MyTable'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pageNumber: 0
+        }
+    }
+
+    /*
+      At work, setting the state in the parent component of the table
+      causes the table to lose its state when changing the page index.
+
+      Theory: When onPageChange calls this.setState and triggers the
+        re-rendering of this component, ReactTable's
+        componentShouldUpdate method is called and probably returns
+        false. This may be telling it to stay at the state that it
+        was at before? Seems like internal state should still have the
+        correct value.
+     */
+
+    onPageChange = (pageIndex) => {
+        console.log('App.onPageChange -> pageIndex: ' + pageIndex)
+        this.setState({pageNumber: pageIndex + 1})
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    Learn React
+                    <p>Page Index: {this.state.pageNumber}</p>
+                    <MyTable onPageChange={this.onPageChange}/>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
